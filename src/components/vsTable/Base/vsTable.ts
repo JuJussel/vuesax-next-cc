7eabc7355f0f08a6c70eebc75939a13c5cbe2591
JuJussel <jujussel@gmail.com>
2020-12-06 15:10:18 +0900

    add adaptive height

--- src/components/vsTable/Base/vsTable.ts
+++ src/components/vsTable/Base/vsTable.ts
@@ -6,18 +6,38 @@ import VsComponent from '../../../mixins/component'
 @Component
 export default class VsTable extends VsComponent {
   colspan: number = 0
+  hsize: number = 0
   @Prop({}) value: any
   @Prop({ default: false, type: Boolean }) striped: any
   @Prop({ default: false, type: Boolean }) loading: boolean
-  // @Prop({ default: false, type: Boolean }) multiple: boolean
+  @Prop({ default: null, type: Array }) adaptive: any
+  @Prop({ default: 40, type: Number }) trHeight: number
 
   mounted() {
     const tds = (this.$refs as any).thead.querySelectorAll('th')
     this.colspan = tds.length
+    if (this.adaptive) {
+      const headerH = (this.$refs as any).theader.querySelector('div').offsetHeight +20
+      const theadH = (this.$refs as any).thead.querySelector('tr').offsetHeight/2
+      this.hsize = headerH + theadH
+    }
   }
 
   get isMultipleSelected() {
     return _.isArray(this.value)
+  }
+
+  get tableHeight() {
+    if (this.adaptive) {
+      let esize = 0
+      const trs = this.adaptive.length
+      if (trs < 1 && this.$refs.notFound) {
+        esize = 200
+      }
+      return trs * this.trHeight + this.hsize + esize
+    } else {
+      return 0
+    }
   }
 
   selected(val: any) {
@@ -46,6 +66,7 @@ export default class VsTable extends VsComponent {
     ])
 
     const header = h('header', {
+      ref: "theader",
       staticClass: 'vs-table__header'
     }, [
       this.$slots.header
@@ -59,6 +80,7 @@ export default class VsTable extends VsComponent {
     ])
 
     const notFound = h('tbody', {
+      ref: 'notFound',
       staticClass: 'vs-table_not-found'
     }, [
       h('tr', [
@@ -73,6 +95,7 @@ export default class VsTable extends VsComponent {
     ])
 
     const tbody = h('tbody', {
+      ref: 'tbody',
       staticClass: 'vs-table__tbody'
     }, [
       this.$slots.tbody,
@@ -95,6 +118,9 @@ export default class VsTable extends VsComponent {
 
     return h('div', {
       staticClass: 'vs-table-content',
+      style: {
+        ['height']: this.adaptive ? this.tableHeight + 'px' : 'inherit'
+      }
     }, [
       this.$slots.header && header,
       table,
